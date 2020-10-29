b377e5f93fa818654ca623534781ec04c069b998
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-29 11:13:45 +0530

    [deekshita] Add : Create a table

--- AddressBook.sql
+++ AddressBook.sql
@@ -2,3 +2,17 @@
 create database address_book;                                                   #Create a databse
 use address_book;                                                               #Use database
 select database();                                                              #Select databse
+
+#Usecase2
+create table addressbook_service
+(
+firstName varchar(150) not null,
+lastName varchar(150) not null,
+Address varchar(300),
+city varchar(15),
+state varchar(15),
+zip int unsigned,
+phoneNo int unsigned,
+emailId varchar(30) not null,
+primary key (emailId)
+);
