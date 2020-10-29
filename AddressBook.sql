7ecdd05d254024babfe82434e014e98afd572ba5
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-29 11:02:24 +0530

    [deekshita] Add : Create a database

---
+++ AddressBook.sql
@@ -0,0 +1,4 @@
+#Usecase1
+create database address_book;                                                   #Create a databse
+use address_book;                                                               #Use database
+select database();                                                              #Select databse
