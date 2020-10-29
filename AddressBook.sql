c7115b407291251360d7a7d560ffda9578b1673a
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-29 11:30:26 +0530

    [deekshita] Add : update address book table with name

--- AddressBook.sql
+++ AddressBook.sql
@@ -22,3 +22,7 @@ primary key (emailId)
 insert into addressbook_service(firstName, lastName, Address, city, state, zip, phoneNo, emailId)values
 ('Diya', 'Prakash', '91/160', 'Kanchipuram', 'TamilNadu', '345123', '68246531', 'diya.prakash21@gmail.com'),
 ('Dev', 'Narayan', '4-21', 'Khozikode', 'Kerala', '654231', '918288541', 'narayan1_dev@gmail.com');
+
+#Usecase4
+#Updating contact details with name
+update addressbook_service set city = 'Calicut' where firstName = 'Dev' and lastName = 'Narayan';
