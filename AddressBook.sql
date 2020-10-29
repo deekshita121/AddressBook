2a9c70d11ce9d0845b569b1366775c62cec2db1b
Dee121-ala <deekshitaalavala@gmail.com>
2020-10-29 11:24:46 +0530

    [deekshita] Add : Inserting records

--- AddressBook.sql
+++ AddressBook.sql
@@ -16,3 +16,9 @@ phoneNo int unsigned,
 emailId varchar(30) not null,
 primary key (emailId)
 );
+
+#Usecase3
+#Inserting records
+insert into addressbook_service(firstName, lastName, Address, city, state, zip, phoneNo, emailId)values
+('Diya', 'Prakash', '91/160', 'Kanchipuram', 'TamilNadu', '345123', '68246531', 'diya.prakash21@gmail.com'),
+('Dev', 'Narayan', '4-21', 'Khozikode', 'Kerala', '654231', '918288541', 'narayan1_dev@gmail.com');
